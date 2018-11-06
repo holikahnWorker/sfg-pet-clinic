9463f7fcaeeb1db3b8df1b96883fe6d2a2a75e17
holikahnWorker <holikahnworker@gmail.com>
2018-11-06 18:11:06 +0100

    Adding Pjos's. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firtsName;
+    private String lastName;
+
+    public String getFirtsName() {
+        return firtsName;
+    }
+
+    public void setFirtsName(String firtsName) {
+        this.firtsName = firtsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
